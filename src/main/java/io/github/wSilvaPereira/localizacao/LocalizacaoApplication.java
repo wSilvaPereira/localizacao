07b6b09c4ea2d3d9d0d51de8ec39d680a732c34e
williampereira84@gmail.com <wSilvaPereira>
2022-08-24 12:45:38 -0300

    Commit Inicial

---
+++ src/main/java/io/github/wSilvaPereira/localizacao/LocalizacaoApplication.java
@@ -0,0 +1,19 @@
+package io.github.wSilvaPereira.localizacao;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalizacaoApplication implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+
+
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalizacaoApplication.class, args);
+	}
+}
---
+++ src/main/java/io/github/wSilvaPereira/localizacao/domain/entity/Cidade.java
@@ -0,0 +1,4 @@
+package io.github.wSilvaPereira.localizacao.domain.entity;
+
+public class Cidade {
+}
