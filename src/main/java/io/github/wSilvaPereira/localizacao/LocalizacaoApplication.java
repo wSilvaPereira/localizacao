3d72a23f67c5c50363d106fecd3c2d7fe12a5145
williampereira84@gmail.com <wSilvaPereira>
2022-08-25 08:41:39 -0300

    Treinando query Methods

--- src/main/java/io/github/wSilvaPereira/localizacao/LocalizacaoApplication.java
+++ src/main/java/io/github/wSilvaPereira/localizacao/LocalizacaoApplication.java
@@ -1,12 +1,10 @@
 package io.github.wSilvaPereira.localizacao;
 
-import io.github.wSilvaPereira.localizacao.domain.entity.Cidade;
 import io.github.wSilvaPereira.localizacao.domain.repository.CidadeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootApplication
 public class LocalizacaoApplication implements CommandLineRunner {
@@ -16,9 +14,7 @@ public class LocalizacaoApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-//		salvarCidade();
-//		listarCidades();
-		listarCidadesPorHabitantes();
+		listarCidadesPorNome();
 	}
 
 	private void listarCidades() {
@@ -26,21 +22,12 @@ public class LocalizacaoApplication implements CommandLineRunner {
 	}
 
 	private void listarCidadesPorNome() {
-		cidadeRepository.findByNome("Curitiba").forEach(System.out::println);
+		cidadeRepository.findByNomeLikeInsensitive("%s%").forEach(System.out::println);
 	}
 
 	private void listarCidadesPorHabitantes() {
 		cidadeRepository.findByHabitantes(12396372L).forEach(System.out::println);
 	}
-
-//	@Transactional
-//	private void salvarCidade() {
-//		var cidade = new Cidade(1L, "São Paulo", 12395372L);
-//		cidadeRepository.save(cidade);
-//
-//		cidade = new Cidade(2L, "Curitiba", 1963726L);
-//		cidadeRepository.save(cidade);
-//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LocalizacaoApplication.class, args);
--- src/main/java/io/github/wSilvaPereira/localizacao/domain/repository/CidadeRepository.java
+++ src/main/java/io/github/wSilvaPereira/localizacao/domain/repository/CidadeRepository.java
@@ -2,13 +2,35 @@ package io.github.wSilvaPereira.localizacao.domain.repository;
 
 import io.github.wSilvaPereira.localizacao.domain.entity.Cidade;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface CidadeRepository extends JpaRepository<Cidade, Long> {
 
+//    Query Methods com o campo Nome(String)
     List<Cidade> findByNome(String nome);
 
+    List<Cidade> findByNomeLike(String nome);
+
+    @Query(" select c from Cidade c where upper(c.nome) like upper(?1) ")
+    List<Cidade> findByNomeLikeInsensitive(String nome);
+
+    List<Cidade> findByNomeStartingWith(String nome);
+
+    List<Cidade> findByNomeEndingWith(String nome);
+
+    List<Cidade> findByNomeContaining(String nome);
+
+//    Query Methods com o campo Habitantes(Numerico)
     List<Cidade> findByHabitantes(Long habitantes);
+    List<Cidade> findByHabitantesLessThan(Long habitantes);
+
+    List<Cidade> findByHabitantesLessThanEqual(Long habitantes);
+    List<Cidade> findByHabitantesGreaterThan(Long habitantes);
+
+    List<Cidade> findByHabitantesGreaterThanEqual(Long habitantes);
+
+    List<Cidade> findByHabitantesAndNomeLike(Long habitantes, String nome);
 
 }
